9c1f2319b0255d4ed238aaf3351ae94b1ba84e60
Joseph McMahon <jmcmahon443@gmail.com>
2016-12-01 22:49:42 -0500

    improved plotting

--- mv_max_plot.py
+++ mv_max_plot.py
@@ -2,25 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-N = 40
-
-def movingaverage(interval, window_size):
-    window = np.ones(int(window_size))/float(window_size)
-    return np.convolve(interval, window, 'same')
+def movingmax(interval):
+    mv_max_mem = 0
+    mv_max = []
+    for i in range(0, len(interval)):
+    	d = interval[i]
+    	if d > mv_max_mem:
+    		mv_max_mem = d
+    		mv_max.append(d)
+    	else:
+    		mv_max.append(mv_max_mem)
+    mv_max = np.array(mv_max)
+    return mv_max
 
 plt.hold(True)
 for file in os.listdir('./analysis'):
     if file.endswith('.csv'):
         d = np.loadtxt(open('analysis/'+file, 'rb'), delimiter=',', skiprows=1)
-        mv_av = movingaverage(d[:,1], N)
+        mv_max = movingmax(d[:,1])
         file = file.replace(' ', '')[:-4].lower()
 
         # plt.plot(d[:,0], d[:,1], marker='o', linestyle='')
-        plt.plot(d[:,0], mv_av, label=file)
+        plt.plot(d[:,0], mv_max, label=file)
 
 # plt.xlim(0, 500)
-plt.title('Moving Average N = '+str(N))
-plt.legend()
+plt.title('Moving Maximum')
+plt.legend(loc=4)
 plt.xlabel('Episode')
 plt.ylabel('Score')
 plt.grid(True)
